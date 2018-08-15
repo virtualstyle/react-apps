237990203c1413fdb1ab8c08a36d030bd4d9c277
virtualstyle <rob@virtualstyle.us>
2018-08-14 20:03:23 -0400

    Add subdirectory that builds from both bundlers

---
+++ react-app-both/webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  entry: './index.js',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'index.js'
+  },
+  devServer: {
+    contentBase: './'
+  }
+};
